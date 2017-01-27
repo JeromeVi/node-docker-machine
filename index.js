b11c4f3d78a3978ac18b47ec1b7588251107bdc5
Jerome <vialj@studentfr.ch>
2017-01-27 09:50:58 +0100

    method kill is added

--- index.js
+++ index.js
@@ -1,476 +1,238 @@
 'use strict';
 
-const test = require('tape')
+const path = require('path')
     , fs = require('fs')
-    , path = require('path')
-    , proxyquire =  require('proxyquire')
-    , spies = []
-    , messages = []
-    , Machine = proxyquire('../', {
-        child_process: createMock(spies),
-        deprecate() {
-          messages.push.apply(messages, arguments)
-        }
+    , env = process.env
+    , cp = require('child_process')
+    , camelCase = require('camel-case')
+    , parallel = require('run-parallel-limit')
+    , xtend = require('xtend')
+    , deprecate = require('deprecate')
+
+const HOST_NON_EXISTENT = /host does not exist/i
+    , ALREADY_RUNNING = /already running/i
+    , ALREADY_STOPPED = /already stopped/
+    , NEWLINE = /\r?\n/
+    , LIST_COLUMNS_SEP = ','
+
+const LIST_COLUMNS
+  = [ 'Name'
+    , 'Active'
+    , 'ActiveHost'
+    , 'ActiveSwarm'
+    , 'DriverName'
+    , 'State'
+    , 'URL'
+    , 'Swarm'
+    , 'Error'
+    , 'DockerVersion'
+    , 'ResponseTime' ]
+
+class Machine {
+  constructor(opts) {
+    opts = Machine.options(opts)
+    this.name = opts.name || env.DOCKER_MACHINE_NAME || 'default'
+  }
+
+  static options(opts) {
+    if (typeof opts === 'string') return { name: opts }
+    else return opts || {}
+  }
+
+  static command(args, done) {
+    cp.execFile('docker-machine', [].concat(args), {
+      cwd: env.DOCKER_TOOLBOX_INSTALL_PATH || '.',
+      encoding: 'utf8'
+    }, done)
+  }
+
+  static status(name, done) {
+    Machine.command(['status', name], (err, stdout) => {
+      if (err) done(err)
+      else done(null, stdout.trim().toLowerCase())
+    })
+  }
+
+  static isRunning(name, done) {
+    Machine.status(name, (err, status) => {
+      done(err, status === 'running')
+    })
+  }
+
+  static start(name, done) {
+    Machine.command(['start', name], (err) => {
+      if (HOST_NON_EXISTENT.test(err)) {
+        done(new Error(`Docker host "${name}" does not exist`))
+      } else if (ALREADY_RUNNING.test(err)) {
+        done()
+      } else {
+        done(err)
+      }
+    })
+  }
+
+  static stop(name, done) {
+    Machine.command(['stop', name], (err) => {
+      if (HOST_NON_EXISTENT.test(err)) {
+        done(new Error(`Docker host "${name}" does not exist`))
+      } else if (ALREADY_STOPPED.test(err)) {
+        done()
+      } else {
+        done(err)
+      }
+    })
+  }
+
+  static kill(name, done) {
+    Machine.command(['kill', name], (err) => {
+      if (HOST_NON_EXISTENT.test(err)) {
+        done(new Error(`Docker host "${name}" does not exist`))
+      } else if (ALREADY_STOPPED.test(err)) {
+        done()
+      } else {
+        done(err)
+      }
+    })
+  }    
+    
+  static env(name, opts, done) {
+    if (typeof opts === 'function') done = opts, opts = {}
+
+    const args = ['env']
+
+    if (opts.json) {
+      deprecate(
+        'The "json" option has been renamed to "parse" and',
+        'will be removed in node-docker-machine v3.x.x.'
+      )
+
+      opts = xtend(opts, { parse: true })
+    }
+
+    if (opts.parse) args.push('--shell', 'bash')
+    else if (opts.shell) args.push('--shell', opts.shell)
+
+    args.push(name)
+
+    Machine.command(args, function (err, stdout) {
+      if (err) return done(err)
+      if (!opts.parse) return done(null, stdout.trim())
+
+      const res = {}
+
+      stdout.split(/\n+/).forEach(line => {
+        const m = /^export (.+)="([^"]+)/i.exec(line)
+        if (m) res[m[1]] = m[2]
       })
 
-test('name defaults to DOCKER_MACHINE_NAME or "default"', function (t) {
-  t.plan(6)
-
-  process.env.DOCKER_MACHINE_NAME = 'beep'
-
-  t.is(new Machine().name, 'beep', 'DOCKER_MACHINE_NAME')
-  t.is(new Machine('agent').name, 'agent', 'as string')
-  t.is(new Machine({ name: 'agent' }).name, 'agent', 'as option')
-
-  process.env.DOCKER_MACHINE_NAME = ''
-
-  t.is(new Machine().name, 'default', 'default')
-  t.is(new Machine('agent').name, 'agent', 'as string')
-  t.is(new Machine({ name: 'agent' }).name, 'agent', 'as option')
-})
-
-test('cwd defaults to DOCKER_TOOLBOX_INSTALL_PATH or cwd', function (t) {
-  t.plan(6)
-
-  process.env.DOCKER_TOOLBOX_INSTALL_PATH = '/docker'
-
-  const s1 = spy({ result: 'fake1' })
-  const s2 = spy({ result: 'fake2' })
-
-  Machine.command('anything1', (err, res) => {
-    t.is(res, 'fake1')
-    t.same(s1.args, ['anything1'])
-    t.is(s1.opts.cwd, '/docker', 'DOCKER_TOOLBOX_INSTALL_PATH')
-  })
-
-  process.env.DOCKER_TOOLBOX_INSTALL_PATH = ''
-
-  Machine.command('anything2', (err, res) => {
-    t.is(res, 'fake2')
-    t.same(s2.args, ['anything2'])
-    t.is(s2.opts.cwd, '.', 'cwd')
-  })
-})
-
-test('status', function (t) {
-  t.plan(6)
-
-  const s1 = spy({ result: ' running\n' })
-  const s2 = spy({ result: 'Stopped' })
-
-  Machine.status('beep', (err, status) => {
-    t.ifError(err, 'no status error')
-    t.is(status, 'running', 'trimmed')
-    t.same(s1.args, ['status', 'beep'])
-  })
-
-  new Machine().status((err, status) => {
-    t.ifError(err, 'no status error')
-    t.is(status, 'stopped', 'lowercased')
-    t.same(s2.args, ['status', 'default'])
-  })
-})
-
-test('isRunning', function (t) {
-  t.plan(6)
-
-  const s1 = spy({ result: ' running\n' })
-  const s2 = spy({ result: 'Stopped' })
-
-  Machine.isRunning('beep', (err, running) => {
-    t.ifError(err, 'no isRunning error')
-    t.is(running, true, 'running')
-    t.same(s1.args, ['status', 'beep'])
-  })
-
-  new Machine().isRunning((err, running) => {
-    t.ifError(err, 'no isRunning error')
-    t.is(running, false, 'not running')
-    t.same(s2.args, ['status', 'default'])
-  })
-})
-
-test('start', function (t) {
-  t.plan(10)
-
-  const s1 = spy({})
-  const s2 = spy({})
-  const s3 = spy({ error: new Error('\nhost does not exist abc') })
-  const s4 = spy({ error: new Error('\nalready running abc') })
-  const s5 = spy({ error: new Error('other error') })
-
-  Machine.start('beep', (err) => {
-    t.ifError(err, 'no start error')
-    t.same(s1.args, ['start', 'beep'])
-  })
-
-  new Machine().start(err => {
-    t.ifError(err, 'no start error')
-    t.same(s2.args, ['start', 'default'])
-  })
-
-  Machine.start('boop', (err) => {
-    t.is(err.message, 'Docker host "boop" does not exist', 'non existent error')
-    t.same(s3.args, ['start', 'boop'])
-  })
-
-  new Machine().start(err => {
-    t.ifError(err, 'no start error if already started')
-    t.same(s4.args, ['start', 'default'])
-  })
-
-  Machine.start('four', (err) => {
-    t.is(err.message, 'other error', 'passthrough other error')
-    t.same(s5.args, ['start', 'four'])
-  })
-})
-
-test('stop', function (t) {
-  t.plan(10)
-
-  const s1 = spy({})
-  const s2 = spy({})
-  const s3 = spy({ error: new Error('\nhost does not exist abc') })
-  const s4 = spy({ error: new Error('\nalready stopped abc') })
-  const s5 = spy({ error: new Error('other error') })
-
-  Machine.stop('beep', (err) => {
-    t.ifError(err, 'no stop error')
-    t.same(s1.args, ['stop', 'beep'])
-  })
-
-  new Machine().stop(err => {
-    t.ifError(err, 'no stop error')
-    t.same(s2.args, ['stop', 'default'])
-  })
-
-  Machine.stop('boop', (err) => {
-    t.is(err.message, 'Docker host "boop" does not exist', 'non existent error')
-    t.same(s3.args, ['stop', 'boop'])
-  })
-
-  new Machine().stop(err => {
-    t.ifError(err, 'no stop error if already stopped')
-    t.same(s4.args, ['stop', 'default'])
-  })
-
-  Machine.stop('four', (err) => {
-    t.is(err.message, 'other error', 'passthrough other error')
-    t.same(s5.args, ['stop', 'four'])
-  })
-})
-
-test('kill', function (t) {
-  t.plan(10)
-
-  const s1 = spy({})
-  const s2 = spy({})
-  const s3 = spy({ error: new Error('\nhost does not exist abc') })
-  const s4 = spy({ error: new Error('\nalready stopped abc') })
-  const s5 = spy({ error: new Error('other error') })
-
-  Machine.kill('beep', (err) => {
-    t.ifError(err, 'no kill error')
-    t.same(s1.args, ['kill', 'beep'])
-  })
-
-  new Machine().kill(err => {
-    t.ifError(err, 'no kill error')
-    t.same(s2.args, ['kill', 'default'])
-  })
-
-  Machine.kill('boop', (err) => {
-    t.is(err.message, 'Docker host "boop" does not exist', 'non existent error')
-    t.same(s3.args, ['kill', 'boop'])
-  })
-
-  new Machine().kill(err => {
-    t.ifError(err, 'no kill error if already stopped')
-    t.same(s4.args, ['kill', 'default'])
-  })
-
-  Machine.kill('four', (err) => {
-    t.is(err.message, 'other error', 'passthrough other error')
-    t.same(s5.args, ['kill', 'four'])
-  })
-})
-
-test('env', function (t) {
-  t.plan(6)
-
-  const s1 = spy({ result: fixture('env-windows.txt') })
-  const s2 = spy({ result: fixture('env-bash.txt') })
-
-  Machine.env('beep', (err, result) => {
-    t.ifError(err, 'no env error')
-    t.is(result, s1.result)
-    t.same(s1.args, ['env', 'beep'])
-  })
-
-  new Machine().env((err, result) => {
-    t.ifError(err, 'no env error')
-    t.is(result, s2.result)
-    t.same(s2.args, ['env', 'default'])
-  })
-})
-
-test('env as plain object', function (t) {
-  t.plan(6)
-
-  const s1 = spy({ result: fixture('env-bash.txt') })
-  const s2 = spy({ result: fixture('env-bash.txt') })
-
-  Machine.env('beep', { parse: true }, (err, result) => {
-    t.ifError(err, 'no env error')
-    t.same(result, {
-      DOCKER_TLS_VERIFY: '1',
-      DOCKER_HOST: 'tcp://<ip>:<port>',
-      DOCKER_CERT_PATH: '<home>/.docker/machine/machines/<name>',
-      DOCKER_MACHINE_NAME: '<name>'
+      done(null, res)
     })
-    t.same(s1.args, ['env', '--shell', 'bash', 'beep'])
-  })
+  }
 
-  new Machine().env({ parse: true }, (err, result) => {
-    t.ifError(err, 'no env error')
-    t.same(result, {
-      DOCKER_TLS_VERIFY: '1',
-      DOCKER_HOST: 'tcp://<ip>:<port>',
-      DOCKER_CERT_PATH: '<home>/.docker/machine/machines/<name>',
-      DOCKER_MACHINE_NAME: '<name>'
-    })
-    t.same(s2.args, ['env', '--shell', 'bash', 'default'])
-  })
-})
-
-test('env with json option is deprecated', function (t) {
-  t.plan(4)
-
-  const s1 = spy({ result: fixture('env-bash.txt') })
-
-  Machine.env('beep', { json: true }, (err, result) => {
-    t.ifError(err, 'no env error')
-    t.same(result, {
-      DOCKER_TLS_VERIFY: '1',
-      DOCKER_HOST: 'tcp://<ip>:<port>',
-      DOCKER_CERT_PATH: '<home>/.docker/machine/machines/<name>',
-      DOCKER_MACHINE_NAME: '<name>'
-    })
-    t.same(s1.args, ['env', '--shell', 'bash', 'beep'])
-    t.same(messages.splice(0), [
-      'The "json" option has been renamed to "parse" and',
-      'will be removed in node-docker-machine v3.x.x.'
-    ], 'deprecated')
-  })
-})
-
-test('env with custom shell', function (t) {
-  t.plan(2)
-
-  const s1 = spy({ result: '' })
-
-  Machine.env('beep', { shell: 'fish' }, (err, result) => {
-    t.ifError(err, 'no env error')
-    t.same(s1.args, ['env', '--shell', 'fish', 'beep'])
-  })
-})
-
-test('env as plain object overrides custom shell', function (t) {
-  t.plan(3)
-
-  const s1 = spy({ result: fixture('env-bash.txt') })
-
-  Machine.env('beep', { parse: true, shell: 'fish' }, (err, result) => {
-    t.ifError(err, 'no env error')
-    t.same(s1.args, ['env', '--shell', 'bash', 'beep'])
-    t.same(result, {
-      DOCKER_TLS_VERIFY: '1',
-      DOCKER_HOST: 'tcp://<ip>:<port>',
-      DOCKER_CERT_PATH: '<home>/.docker/machine/machines/<name>',
-      DOCKER_MACHINE_NAME: '<name>'
-    })
-  })
-})
-
-test('ssh', function (t) {
-  t.plan(8)
-
-  const s1 = spy({ result: ' beep ' })
-  const s2 = spy({ result: ' boop ' })
-
-  Machine.ssh('beep', 'cmd', (err, result) => {
-    t.ifError(err, 'no ssh error')
-    t.is(result, s1.result, 'does not trim')
-    t.same(s1.args, ['ssh', 'beep', 'cmd'])
-  })
-
-  new Machine().ssh('cmd', (err, result) => {
-    t.ifError(err, 'no ssh error')
-    t.is(result, s2.result, 'does not trim')
-    t.same(s2.args, ['ssh', 'default', 'cmd'])
-  })
-
-  t.throws(() => {
-    Machine.ssh('beep', null, () => t.fail('Should not be called'))
-  }, 'command cannot be null')
-
-  t.throws(() => {
-    Machine.ssh('beep', '  ', () => t.fail('Should not be called'))
-  }, 'command cannot be empty')
-})
-
-test('ssh with command as array', function (t) {
-  t.plan(5)
-
-  const s1 = spy({ })
-  const s2 = spy({ })
-
-  Machine.ssh('beep', ['cmd', '--flag'], (err, result) => {
-    t.ifError(err, 'no ssh error')
-    t.same(s1.args, ['ssh', 'beep', 'cmd --flag'])
-  })
-
-  new Machine().ssh(['cmd', '--flag'], (err, result) => {
-    t.ifError(err, 'no ssh error')
-    t.same(s2.args, ['ssh', 'default', 'cmd --flag'])
-  })
-
-  t.throws(() => {
-    Machine.ssh('beep', ['', ''], () => t.fail('Should not be called'))
-  }, 'command cannot be empty')
-})
-
-test('inspect', function (t) {
-  t.plan(9)
-
-  const s1 = spy({ result: JSON.stringify({ name: 'beep' }) + '\r\n' })
-  const s2 = spy({ result: JSON.stringify({ Other_Field: 'boop' }) })
-  const s3 = spy({ result: 'invalid json' })
-
-  Machine.inspect('beep', (err, result) => {
-    t.ifError(err, 'no inspect error')
-    t.same(result, { name: 'beep' }, 'parses trimmed JSON')
-    t.same(s1.args, ['inspect', 'beep'])
-  })
-
-  new Machine().inspect((err, result) => {
-    t.ifError(err, 'no inspect error')
-    t.same(result, { otherField: 'boop' }, 'camelcased')
-    t.same(s2.args, ['inspect', 'default'])
-  })
-
-  new Machine().inspect((err, result) => {
-    t.ok(err, 'catches JSON parse error')
-    t.is(result, undefined, 'no result')
-    t.same(s2.args, ['inspect', 'default'])
-  })
-})
-
-test('list', function (t) {
-  t.plan(4)
-
-  const s1 = spy({ result: fixture('list.txt') })
-
-  t.is(Machine.prototype.list, undefined, 'static-only method')
-
-  Machine.list((err, result) => {
-    t.ifError(err, 'no inspect error')
-    t.same(result, [
-      { active: '*'
-      , activeHost: true
-      , activeSwarm: false
-      , dockerVersion: 'v1.11.2'
-      , driverName: 'virtualbox'
-      , error: null
-      , name: 'default'
-      , responseTime: 290
-      , state: 'running'
-      , swarm: null
-      , url: 'tcp://192.168.99.100:2376' },
-      { active: '-'
-      , activeHost: false
-      , activeSwarm: false
-      , dockerVersion: null
-      , driverName: 'virtualbox'
-      , error: null
-      , name: 'host-1'
-      , responseTime: 327
-      , state: 'stopped'
-      , swarm: null
-      , url: 'tcp://192.168.99.101:2376' },
-      { active: '* (swarm)'
-      , activeHost: true
-      , activeSwarm: true
-      , dockerVersion: 'v1.11.0'
-      , driverName: 'virtualbox'
-      , error: null
-      , name: 'host-2'
-      , responseTime: 405
-      , state: 'running'
-      , swarm: null
-      , url: 'tcp://192.168.99.102:2376' }
-    ])
-    t.same(s1.args, ['ls', '-f', fixture('template.txt')])
-  })
-})
-
-test('list with inspect', function (t) {
-  t.plan(9)
-
-  const s1 = spy({ result: fixture('list.txt') })
-
-  const s2 = spy({ result: JSON.stringify({ Extra: { Options: { a: true, b: '' } } }) })
-  const s3 = spy({ result: JSON.stringify({ SOME_PROPERTY: 'hello' }) })
-  const s4 = spy({ result: JSON.stringify({ other: 5 }) })
-
-  Machine.list({ inspect: true }, (err, result) => {
-    t.ifError(err, 'no inspect error')
-
-    t.same(s1.args, ['ls', '-f', fixture('template.txt')])
-
-    t.same(s2.args, ['inspect', 'default'], 'inspect default')
-    t.same(s3.args, ['inspect', 'host-1'], 'inspect host-1')
-    t.same(s4.args, ['inspect', 'host-2'], 'inspect host-2')
-
-    t.is(result.length, 3, '3 hosts')
-
-    t.same(result[0].extra, { options: { a: true, b: '' }})
-    t.same(result[1].someProperty, 'hello')
-    t.same(result[2].other, 5)
-  })
-})
-
-test('list with timeout', function (t) {
-  t.plan(2)
-
-  const s1 = spy({ result: '' })
-
-  Machine.list({ timeout: 5 }, (err, result) => {
-    t.ifError(err, 'no inspect error')
-    t.same(s1.args, ['ls', '-f', fixture('template.txt'), '-t', '5'])
-  })
-})
-
-function spy(state) {
-  spies.push(state)
-  return state
-}
-
-function createMock(spies) {
-  return {
-    execFile(cmd, args, opts, done) {
-      const state = spies.shift()
-
-      state.cmd = cmd
-      state.args = args
-      state.opts = opts
-
-      process.nextTick(done, state.error || null, state.result)
+  static ssh(name, cmd, done) {
+    if (Array.isArray(cmd)) {
+      cmd = cmd.join(' ')
+    } else if (typeof cmd !== 'string') {
+      throw new TypeError('Command must be an array or string')
     }
+
+    cmd = cmd.trim()
+    if (!cmd) throw new TypeError('Command may not be empty')
+
+    Machine.command(['ssh', name, cmd], done)
+  }
+
+  static inspect(name, done) {
+    Machine.command(['inspect', name], (err, stdout) => {
+      if (err) return done(err)
+
+      try {
+        var data = JSON.parse(stdout.trim())
+      } catch (err) {
+        return done(err)
+      }
+
+      done(null, merge({}, data))
+    })
+  }
+
+  static list(opts, done) {
+    if (typeof opts === 'function') done = opts, opts = {}
+
+    // Build template, escape values with URL encoding
+    const template = LIST_COLUMNS.map(name => {
+      if (name === 'ResponseTime') {
+        return `{{ .${name} | printf "%d" }}`
+      } else {
+        return `{{ .${name} | urlquery }}`
+      }
+    }).join(LIST_COLUMNS_SEP)
+
+    const args = ['ls', '-f', template]
+
+    // Optionally add a timeout (in seconds)
+    // to deal with docker/machine#1696.
+    if (opts.timeout) args.push('-t', String(opts.timeout))
+
+    Machine.command(args, (err, stdout) => {
+      if (err) return done(err)
+
+      const machines = stdout.split(NEWLINE).filter(Boolean).map(line => {
+        const values = line.split(LIST_COLUMNS_SEP)
+            , machine = {}
+
+        LIST_COLUMNS.forEach((name, i) => {
+          const key = camelCase(name)
+          const val = values[i]
+
+          machine[key] = val === '' ? null : decodeURIComponent(val)
+        })
+
+        // ResponseTime is in nanoseconds
+        machine.responseTime = parseInt(machine.responseTime) / 1e6
+        machine.state = machine.state.toLowerCase()
+        machine.activeHost = machine.activeHost === 'true'
+        machine.activeSwarm = machine.activeSwarm === 'true'
+
+        if (machine.dockerVersion === 'Unknown') {
+          machine.dockerVersion = null
+        }
+
+        return machine
+      })
+
+      if (!opts.inspect) return done(null, machines)
+
+      // Add additional metadata from `docker-machine inspect <name>`
+      parallel(machines.map(machine => next => {
+        Machine.inspect(machine.name, (err, data) => {
+          if (err) next(err)
+          else next(null, xtend(machine, data))
+        })
+      }), 4, done)
+    })
   }
 }
 
-function fixture(name) {
-  const file = path.join(__dirname, 'fixture', name)
-  return fs.readFileSync(file, 'utf8').trim()
+;['status', 'isRunning', 'start', 'stop', 'kill', 'env', 'ssh', 'inspect'].forEach(method => {
+  Machine.prototype[method] = function () {
+    const args = Array.from(arguments)
+    args.unshift(this.name)
+    Machine[method].apply(null, args)
+  }
+})
+
+module.exports = Machine
+
+function merge(node, data) {
+  for(let key in data) {
+    const val = data[key]
+    node[camelCase(key)] = isObject(val) ? merge({}, val) : val
+  }
+
+  return node
+}
+
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null
 }
